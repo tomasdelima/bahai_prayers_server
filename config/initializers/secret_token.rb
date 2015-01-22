f1b7dc195ed71e2c7fac7b489111d1bfef242b39
Tomas Duringer Jacques de Lima <tomas.lima@dtmtec.com.br>
2015-01-22 19:20:44 -0200

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BahaiPrayersServer::Application.config.secret_key_base = 'a1474cd2b80958ac6d061dea54747e2656c4818f537cd74f4de92919bfad032a1ed75e97edc840f330eb50173bba247723abd9fa8a03afc2e0725e17916eded4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BahaiPrayersServer::Application.config.session_store :cookie_store, key: '_bahai-prayers-server_session'
