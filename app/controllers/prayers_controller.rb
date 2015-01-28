d465bd7697a3e85261b5423442e6c566642e3bb5
tomasdelima <tomasdelima@gmail.com>
2015-01-28 21:48:03 -0200

    Corrige controller de orações

--- app/controllers/prayers_controller.rb
+++ app/controllers/prayers_controller.rb
@@ -1,5 +1,10 @@
 class PrayersController < ApplicationController
   def index
-    render json: Prayer.where(category_id: params[:category_id]).to_json
+    if params[:category_id]
+      prayers = Prayer.where(category_id: params[:category_id])
+    else
+      prayers = Prayer.all
+    end
+    render json: prayers.to_json
   end
 end
