bba8fe563b65770ebe6b3d59d9f1d67a0efa44a7
tomasdelima <tomasdelima@gmail.com>
2015-02-13 01:36:26 -0200

    Serve orações com quebras de linha em HTML

--- app/controllers/prayers_controller.rb
+++ app/controllers/prayers_controller.rb
@@ -3,15 +3,15 @@ class PrayersController < ApplicationController
     prayers = Prayer.all
     prayers = prayers.where("updated_at > '#{Time.at(Integer(params[:last_updated_at])/1000)}'") if params[:last_updated_at]
     prayers = prayers.where(category_id: params[:category_id]) if params[:category_id]
+    prayers.each{|p| p.body = p.body.gsub(/(\n)+/, "<br><br>").html_safe}
+
     render json: prayers.to_json
   end
 
   def update
     if Prayer.find(params[:id]).update_attribute(params[:key], params[:value])
-      puts "-----", "Saved prayer: #{params[:key]} => #{params[:value]}", "-----"
       render json: {message: 'Oração salva com sucesso'}
     else
-      puts "-----", "Error saving prayer: #{params[:key]} => #{params[:value]}", "-----"
       render json: {message: 'Erro ao salvar a oração'}
     end
   end
