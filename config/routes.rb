e24284be8fd0d7ff5b2bfc3d55cc5589ee91a79f
Tomás de Lima <tomasdelima@gmail.com>
2016-08-05 18:06:33 -0300

    Configures ActiveAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 BahaiPrayersServer::Application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :prayers, only: :index
   resources :categories, only: [:index, :show] do
     resources :prayers
   end
+  resources :books, only: [:index, :show]
 
   get  'mobile_errors',      to: 'mobile_errors#create'
   post 'categories',         to: 'categories#update'
   post 'prayers',            to: 'prayers#update'
   post 'categories/prayers', to: 'prayers#update'
+  post 'book',               to: 'books#update'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308161905) do
+ActiveRecord::Schema.define(version: 20160805132554) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "categories", force: true do |t|
-    t.string   "title"
+  create_table "active_admin_comments", force: true do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.integer  "author_id"
+    t.string   "author_type"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "active",     default: true
   end
+
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+
+  create_table "books", force: true do |t|
+    t.boolean  "active"
+    t.string   "title"
+    t.text     "body"
+    t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+# Could not dump table "categories" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
   create_table "mobile_errors", force: true do |t|
     t.text     "message"
@@ -30,17 +47,8 @@ ActiveRecord::Schema.define(version: 20150308161905) do
     t.datetime "updated_at"
   end
 
-  create_table "prayers", force: true do |t|
-    t.string   "author"
-    t.text     "body"
-    t.integer  "category_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.text     "preamble"
-    t.boolean  "active",      default: true
-  end
-
-  add_index "prayers", ["category_id"], name: "index_prayers_on_category_id", using: :btree
+# Could not dump table "prayers" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -51,13 +59,13 @@ ActiveRecord::Schema.define(version: 20150308161905) do
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
